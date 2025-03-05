de850f94402ef12ed88502ebcd81816f1405aacf
carl-thilander <carl.thilander@medieinstitutet.se>
2025-03-05 09:45:43 +0100

    Installed all the MUI files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
     <main>
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+      <Button variant="outlined">Hello World</Button>
     </main>
   );
 }
