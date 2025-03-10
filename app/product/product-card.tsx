ff6218a976260b4a7e19a3512dac4e04c4e071ba
Anna <anna.kvernplassen@gmail.com>
2025-03-10 12:36:51 +0100

    start layout the product card

---
+++ app/product/product-card.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { products } from "@/data";
+import { Product } from "@/data";
+import Link from "next/link";
+
+export default function ProductCard() {
+  return (
+   
+    {products.map((product:Product) => (
+      <Link href="/product/page" key={product.id}>
+    <Card sx={{ maxWidth: 345 }} >
+      <CardMedia sx={{ height: 140 }} image="" title={product.title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {product.title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {product.description}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {product.price} kr
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Köp</Button>
+      </CardActions>
+    </Card>
+    </Link>
+    ))}
+  );
+}
