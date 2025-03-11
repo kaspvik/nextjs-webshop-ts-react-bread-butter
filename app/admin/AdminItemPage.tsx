d249a4213f79826f74063cc23cafc4fbe10393c9
carl-thilander <carl.thilander@medieinstitutet.se>
2025-03-11 19:37:49 +0100

    Started cretaing adminitem component

---
+++ app/admin/AdminItemPage.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, Container } from "@mui/material";
+import { Product } from "@prisma/client";
+
+export default function AdminItem(product: Product){
+    return (
+        <Container key={product.id} sx={{
+            display:"flex",
+            gap:"4",
+            justifyContent:"space-between",
+        }}>
+            <Box>
+                <img src={product.image}/>
+            </Box>
+
+            <Box>
+                <h4>{product.title}</h4>
+                <p>{product.articleNumber}</p>
+                <p>{product.price}</p>
+                <p>{product.description}</p>
+                <textarea name="Beskrivning" id="Textbox" placeholder="Skriv information om produkten här"></textarea>
+            </Box>
+            <Box sx={{
+                display: "flex",
+                flexDirection:"column",
+                gap:"2",
+                justifyContent:"space-between"
+            }}>
+                
+        <Button
+            size="small"
+            sx={{
+              backgroundColor: "#9C8173",
+              color: "#FAF2E9",
+              mx: "auto",
+              padding: 1,
+              minWidth: "90px",
+              
+            }}
+          >
+            REDIGERA
+        </Button>
+
+        <Button
+            size="small"
+            sx={{
+            backgroundColor: "#FAF2E9",
+            color: "#9C8173",
+            mx: "auto",
+            padding: 1,
+            minWidth: "90px",
+            margin: 1,
+            }}
+            >
+            TA BORT
+        </Button>
+
+            </Box>
+
+            
+        </Container>
+    )
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,11 @@
+import { Container } from "@mui/material";
+import AdminItem from "./AdminItemPage";
+
 export default function AdminPage(){
     return (
+      <Container>
         <h2>AdminPage</h2>
+        <AdminItem/>
+        </Container>
     )
 }
