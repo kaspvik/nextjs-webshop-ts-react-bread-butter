428a86e9674899de8dbae8fbdbd8f85810e1997d
Carl-Thilandergit <carl.thilander@medieinstitutet.se>
2025-03-14 14:49:13 +0100

    Changes in seedinig and prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { products } from '@/data';
-import { db } from './db';
+import { products } from "@/data";
+import { db } from "./db";
 
 async function main() {
   for (const { id, ...product } of products) {
