8c38df1991b331e66ee6e07ac8ad9674ebd5a588
Carl-Thilandergit <carl.thilander@medieinstitutet.se>
2025-03-14 09:08:51 +0100

    Prettier styling commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,14 +3,22 @@ import { Container } from "@mui/material";
 import AddProductButton from "./add-product-item";
 import AdminItem from "./admin-item";
 
-export default async function AdminPage(){
+export default async function AdminPage() {
   const products = await db.product.findMany();
-    return (
-      <Container sx={{ display: "flex", flexDirection: "column", gap: 2, bgcolor: "background.paper", marginTop: 2 }}>
-        <AddProductButton/>
-        {products.map((product) => (
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        bgcolor: "background.paper",
+        marginTop: 2,
+      }}
+    >
+      <AddProductButton />
+      {products.map((product) => (
         <AdminItem key={product.id} product={product} />
       ))}
-        </Container>
-    )
+    </Container>
+  );
 }
