48b1a25ae81fe12ecf55e6e95fc421d04843af50
Anna <anna.kvernplassen@gmail.com>
2025-03-19 16:45:22 +0100

    måsvinge2

--- app/admin/action.ts
+++ app/admin/action.ts
@@ -121,17 +121,4 @@ export async function getOrderByOrderNr(orderNr: string) {
     console.error("Error fetching order:", error);
     throw new Error("Failed to fetch order");
   }
-
-
-export async function updateProduct(
-  articleNumber: string,
-  data: Prisma.ProductUpdateInput
-) {
-  await db.product.update({
-    where: { articleNumber },
-    data,
-  });
-  revalidatePath("/admin");
-  redirect("/admin");
-
 }
