65f5b82344bb049b07bc23be76b86a3dc64bf7fb
Kasper Vikström <kasper.vik@hotmail.com>
2025-05-21 10:18:15 +0200

    update language and typography variants on admin

--- app/admin/admin-item-card.tsx
+++ app/admin/admin-item-card.tsx
@@ -11,11 +11,10 @@ type ProductCardProps = {
 export default function AdminItem({ product }: ProductCardProps) {
   return (
     <Container
-      data-cy="product"
       key={product.id}
       sx={{
         display: "flex",
-        flexDirection: { xs:"column",  sm:"row",},
+        flexDirection: { xs: "column", sm: "row" },
         alignItems: "center",
         justifyContent: "space-between",
         backgroundColor: "#FAF2E9",
@@ -23,63 +22,52 @@ export default function AdminItem({ product }: ProductCardProps) {
         borderRadius: 2,
         boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.4)",
         gap: 1,
-        flexWrap: {xs:"wrap", md:"nowrap"}
-      }}
-    >
-      <Box sx={{
-        width: { xs: "100px", md: "150px" },
-        height: { xs: "100px", md: "150px" },
-        
-
+        flexWrap: { xs: "wrap", md: "nowrap" },
       }}>
-      {/* Bild */}
       <Box
-        component="img"
-        src={product.image}
-        alt={product.title}
         sx={{
-          borderRadius: { xs: "50%", md: "20px" },
-          padding: 0.5,
-          border: { xs: "2px solid #9C8173", md: "none" },
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-        }}
-      />
+          width: { xs: "100px", md: "150px" },
+          height: { xs: "100px", md: "150px" },
+        }}>
+        {/* Bild */}
+        <Box
+          component="img"
+          src={product.image}
+          alt={product.title}
+          sx={{
+            borderRadius: { xs: "50%", md: "20px" },
+            padding: 0.5,
+            border: { xs: "2px solid #9C8173", md: "none" },
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
       </Box>
 
       {/* Produktinformation */}
-      <Box sx={{ flex: 1, flexWrap: "wrap",
-        width: {xs: "100%" }
-       }}>
-        <Typography variant="h6" data-cy="product-title">
-          {product.title}
-        </Typography>
+      <Box sx={{ flex: 1, flexWrap: "wrap", width: { xs: "100%" } }}>
+        <Typography variant="h5">{product.title}</Typography>
 
-        <Box sx={{
-          display:"flex",
-          flexDirection:"row",
-        }}>
-        <Typography variant="body2">Art Nr: </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}>
+          <Typography variant="h6">Art Nr: </Typography>
 
-        <Typography variant="body2" data-cy="product-id">
-          {product.articleNumber} 
-        </Typography>
+          <Typography variant="h6">{product.articleNumber}</Typography>
         </Box>
-        <Typography variant="body2">Vikt: {product.weight} g</Typography>
-        <Typography variant="body2" data-cy="product-price">
-          {product.price} kr
-        </Typography>
+        <Typography variant="h6">Vikt: {product.weight} g</Typography>
+        <Typography variant="h6">{product.price} kr</Typography>
 
         {/* Beskrivning */}
-        <Typography variant="subtitle2" sx={{ marginTop: 1 }}>
-          Beskrivning:
+        <Typography variant="h6" sx={{ marginTop: 1 }}>
+          Description:
         </Typography>
         <Typography
-          variant="body2"
-          sx={{ borderColor: "text.secondary", borderRadius: "4px" }}
-          data-cy="product-description"
-        >
+          variant="h6"
+          sx={{ borderColor: "text.secondary", borderRadius: "4px" }}>
           {product.description}
         </Typography>
       </Box>
@@ -88,12 +76,11 @@ export default function AdminItem({ product }: ProductCardProps) {
       <Box
         sx={{
           display: "flex",
-          flexDirection: { xs:"row",  sm:"column",},
-          alignSelf: {xs:"center", sm: "flex-start",},
+          flexDirection: { xs: "row", sm: "column" },
+          alignSelf: { xs: "center", sm: "flex-start" },
           justifyContent: "flex-start",
           gap: 2,
-        }}
-      >
+        }}>
         <EditButton product={product} />
 
         <DeleteButton product={product} />
