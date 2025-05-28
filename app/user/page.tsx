e18fc8c313a3fa9d81eff78d925fb5704e004a5f
Simon <simlin1997@gmail.com>
2025-05-28 15:04:33 +0200

    its working :)))))))))

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,8 +1,28 @@
-export default function UserPage() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-2xl font-bold mb-4">User Page</h1>
-        <p className="text-lg text-gray-700">This is the user page.</p>
-        </div>
-    );
+import { redirect } from "next/navigation";
+import { auth } from "@/app/auth";
+import { db } from "@/prisma/db";
+import { headers } from "next/headers";
+
+export default async function UserPage() {
+  const session = await auth.api.getSession({ headers: headers() });
+
+  if (!session?.user?.id) {
+    redirect("/");
+  }
+
+  const user = await db.user.findUnique({
+    where: { id: session.user.id },
+    select: { isAdmin: true },
+  });
+
+  if (user?.isAdmin) {
+    redirect("/admin");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">User Page</h1>
+      <p className="text-lg text-gray-700">This is the user page.</p>
+    </div>
+  );
 }
