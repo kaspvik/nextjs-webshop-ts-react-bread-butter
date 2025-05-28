fca002efd76a57b081fe7723c934f801931cfdbe
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-05-28 09:06:50 +0200

    edit

--- app/header/nav.tsx
+++ app/header/nav.tsx
@@ -1,31 +1,28 @@
 "use client";
 
-import { Button } from "@mui/material";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { signOut, useSession } from "../auth-client";
+import StyledNetscapeButton from "../components/styled-button";
 import SignInModal from "../signin/signin-modal";
 
 export default function Nav() {
-  const pathname = usePathname();
   const { data } = useSession();
   const [showModal, setShowModal] = useState(false);
-
-  const showSignInButton = pathname !== "/signin";
 
   return (
     <nav className="flex gap-4">
       {data ? (
-        <button onClick={() => signOut()}>Signout</button>
+        <StyledNetscapeButton color="inherit" onClick={() => signOut()}>Signout</StyledNetscapeButton>
       ) : (
-        showSignInButton && (
-          <>
-            <Button color="inherit" onClick={() => setShowModal(true)}>
-              Sign in
-            </Button>
-            {showModal && <SignInModal onClose={() => setShowModal(false)} />}
-          </>
-        )
+        <>
+          <StyledNetscapeButton
+            color="inherit"
+            onClick={() => setShowModal(true)}
+          >
+            Sign in
+          </StyledNetscapeButton>
+          {showModal && <SignInModal onClose={() => setShowModal(false)} />}
+        </>
       )}
     </nav>
   );
