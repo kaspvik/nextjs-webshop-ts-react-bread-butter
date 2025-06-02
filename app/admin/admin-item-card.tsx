90ea96a2ebe1cd144084ac009b6e3ba62242bb3d
Kasper Vikström <kasper.vik@hotmail.com>
2025-06-02 10:04:50 +0200

    Refactor AdminItem component layout and update theme typography styles

--- app/admin/admin-item-card.tsx
+++ app/admin/admin-item-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { updateProductStock } from "@/app/admin/action";
-import { Product } from "@prisma/client";
 import { Box, Button, Container, Snackbar, Typography } from "@mui/material";
+import { Product } from "@prisma/client";
 import { useState, useTransition } from "react";
 import EditButton from "./buttons/edit-admin-button";
 import DeleteButton from "./delete-product-item";
@@ -35,28 +35,23 @@ export default function AdminItem({ product }: ProductCardProps) {
           flexDirection: { xs: "column", sm: "row" },
           alignItems: "center",
           justifyContent: "space-between",
-          backgroundColor: "#FAF2E9",
+          backgroundColor: "#eee",
           padding: 2,
-          borderRadius: 2,
           boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.4)",
           gap: 1,
           flexWrap: { xs: "wrap", md: "nowrap" },
-        }}
-      >
+        }}>
         <Box
           sx={{
             width: { xs: "100px", md: "150px" },
             height: { xs: "100px", md: "150px" },
-          }}
-        >
+          }}>
           <Box
             component="img"
             src={product.image}
-            alt={product.title}
+            alt={product.album}
             sx={{
-              borderRadius: { xs: "50%", md: "20px" },
               padding: 0.5,
-              border: { xs: "2px solid #9C8173", md: "none" },
               width: "100%",
               height: "100%",
               objectFit: "cover",
@@ -65,30 +60,31 @@ export default function AdminItem({ product }: ProductCardProps) {
         </Box>
 
         <Box sx={{ flex: 1, flexWrap: "wrap", width: "100%" }}>
-          <Typography variant="h6">
-            {product.title}
-          </Typography>
+          <Typography variant="h6">{product.album}</Typography>
 
           <Box sx={{ display: "flex", flexDirection: "row" }}>
-            <Typography variant="body2">Art Nr: </Typography>
-            <Typography variant="body2" sx={{ ml: 0.5 }}>
+            <Typography variant="body1" sx={{ fontWeight: 600 }}>
+              Art Nr:{" "}
+            </Typography>
+            <Typography variant="body1" sx={{ ml: 0.5 }}>
               {product.articleNumber}
             </Typography>
           </Box>
 
-          <Typography variant="body2">Artist: {product.artist}</Typography>
-          <Typography variant="body2">Album: {product.album}</Typography>
-          <Typography variant="body2">
-            {product.price} sek
+          <Typography variant="body1" sx={{ fontWeight: 600 }}>
+            Artist: {product.artist}
           </Typography>
+          <Typography variant="body1" sx={{ fontWeight: 600 }}>
+            Album: {product.album}
+          </Typography>
+          <Typography variant="body1">{product.price} sek</Typography>
 
-          <Typography variant="subtitle2" sx={{ marginTop: 1 }}>
+          <Typography variant="body1" sx={{ marginTop: 1, fontWeight: 600 }}>
             Description:
           </Typography>
           <Typography
-            variant="body2"
-            sx={{ borderColor: "text.secondary", borderRadius: "4px" }}
-          >
+            variant="body1"
+            sx={{ borderColor: "text.secondary", borderRadius: "4px" }}>
             {product.description}
           </Typography>
         </Box>
@@ -101,8 +97,7 @@ export default function AdminItem({ product }: ProductCardProps) {
             justifyContent: "center",
             gap: 2,
             minWidth: "140px",
-          }}
-        >
+          }}>
           <Box
             sx={{
               display: "flex",
@@ -110,9 +105,8 @@ export default function AdminItem({ product }: ProductCardProps) {
               gap: 1,
               width: "100%",
               alignItems: "center",
-            }}
-          >
-            <Typography variant="body2" sx={{ fontWeight: 600 }}>
+            }}>
+            <Typography variant="body1" sx={{ fontWeight: 600 }}>
               In stock:
             </Typography>
             <input
@@ -140,8 +134,7 @@ export default function AdminItem({ product }: ProductCardProps) {
                   backgroundColor: "#2b1f16",
                   transform: "scale(1.05)",
                 },
-              }}
-            >
+              }}>
               {isPending ? "Updating..." : "Update"}
             </Button>
           </Box>
@@ -154,8 +147,7 @@ export default function AdminItem({ product }: ProductCardProps) {
               justifyContent: "center",
               alignItems: "center",
               width: "100%",
-            }}
-          >
+            }}>
             <EditButton product={product} />
             <DeleteButton product={product} />
           </Box>
